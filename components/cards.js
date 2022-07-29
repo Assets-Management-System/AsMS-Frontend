d34cf0737940f5e5a4bc5dbed97eed1f4071e55e
IshaDabas <ishadabas@icloud.com>
2022-07-29 16:09:42 +0530

    feat: Implemented Card section in Assets

--- components/cards.js
+++ components/cards.js
@@ -1,36 +1,37 @@
-import React from "react";
-import { ChakraProvider, SimpleGrid, Container } from "@chakra-ui/react";
-import Card from "./Card";
+import React from 'react'
+import { ChakraProvider, SimpleGrid, Container, Box } from '@chakra-ui/react'
+import Card from './layouts/card'
+import AddAssets from './layouts/drawer'
 
 function Cards() {
   const dataList = [
     {
-      id: "1",
-      product: "Product 1",
-      summary: "This is a summary, can be any length",
-      longLine: "Very short, can be any description"
+      id: '1',
+      product: 'Product 1',
+      summary: 'This is a summary, can be any length',
+      longLine: 'Very short, can be any description'
     },
     {
-      id: "2",
-      product: "Product Two",
+      id: '2',
+      product: 'Product Two',
       summary:
-        "Another summary, make sure that this is very responsivesfafsdfsdfsdfsdfsfsfsdf",
-      longLine: "Billy Bob Bob Bob Bob likes Markiplier gameplay videos"
+        'Another summary, make sure that this is very responsivesfafsdfsdfsdfsdfsfsfsdf',
+      longLine: 'Billy Bob Bob Bob Bob likes Markiplier gameplay videos'
     },
     {
-      id: "3",
-      product: "Long Product",
-      summary: "Finalize them summary, hurry, we are close to deadline",
-      longLine: "Wow, this is very descriptive! I wonder how long it is"
+      id: '3',
+      product: 'Long Product',
+      summary: 'Finalize them summary, hurry, we are close to deadline',
+      longLine: 'Wow, this is very descriptive! I wonder how long it is'
     }
-  ];
+  ]
 
   return (
     <ChakraProvider>
-      <Container maxW="80rem" centerContent>
+      <Container maxW="80rem" pt={10} centerContent>
         <SimpleGrid columns={[1, 2, 1, 2]}>
           {dataList.map(function (data) {
-            const { id, product, summary, longLine } = data;
+            const { id, product, summary, longLine } = data
             return (
               <Card
                 key={id}
@@ -38,12 +39,15 @@ function Cards() {
                 summary={summary}
                 longLine={longLine}
               />
-            );
+            )
           })}
+          <Box p={4} maxWidth="32rem" borderWidth={2} margin={2}>
+            <AddAssets></AddAssets>
+          </Box>
         </SimpleGrid>
       </Container>
     </ChakraProvider>
-  );
+  )
 }
 
 export default Cards
--- pages/assets.js
+++ pages/assets.js
@@ -1,8 +1,13 @@
 import Selection from "../components/selection"
+import Cards from "../components/cards"
+import { Box } from "@chakra-ui/react"
 
 const asset = () => {
     return (
-        <Selection> </Selection>
+        <Box>
+        <Selection></Selection>
+        <Cards></Cards>
+        </Box>
     )
 }
 
